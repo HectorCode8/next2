013c336acd652a65ce2cd0f5d0f81da392dff5e4
Hector Haro H <hector8.haro@gmail.com>
2022-04-24 22:50:46 -0500

    add getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 
-
-export default function Home() {
+const Pokemon = ({ pokemon }) => {
   return (
-    <div>Pokemones</div>
+    <li>{pokemon.name}</li>
   )
+}
+
+export default function Pokemones({ pokemones }) {
+  return (
+    <div>
+      <p>Pokemones</p>
+      <ul>
+        {pokemones.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name} />)}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const data = await res.json()
+  return {
+    props: {
+      pokemones: data.results
+    }
+  }
 }
