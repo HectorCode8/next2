ca12b4f19b0e603b5c29e7213b3997b512cb2001
Hector Haro H <hector8.haro@gmail.com>
2022-04-24 23:05:35 -0500

    Add getServerSideProps method

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,8 +1,16 @@
 
-const Pokemon = () => {
+const Pokemon = ( { data }) => {
     return (
         <p>Lala</p>
     )
 }
 
 export default Pokemon
+
+export const getServerSideProps = async ({ params }) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+    const data = await res.json()
+
+
+    return { props: { data } }
+}
