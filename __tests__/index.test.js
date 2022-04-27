74f3ebfb93aa8081342f80c2679df547e5c2f4e7
Hector Haro H <hector8.haro@gmail.com>
2022-04-26 23:08:21 -0500

    add __tests__ folder

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+const add = () => 2 + 2
+
+describe('Index', () => {
+
+    describe('Components', () => {
+        it('se renderiza', () => {
+            
+        })
+    })
+    it('suma 2 + 2', () => {
+        expect(add()).toBe(4);
+    })
+})
